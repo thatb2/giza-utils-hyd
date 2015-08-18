e61f4a2c74218d96f87d949e4e2b1312988e238c
thatb2 <Bhargava.Thatikonda@in.pega.com>
2015-08-18 14:25:39 +0530

    Update run_rspec_andr.sh

--- jenkins_jobs/run_rspec_andr.sh
+++ jenkins_jobs/run_rspec_andr.sh
@@ -88,10 +88,11 @@ set_up_global_properties PROPERTIES
 
 start_appium
 cd "$GIZA_HOME"
-PATH_TO_GEMFILE=$(dirname $RSPEC_FILE_PATH)
+PATH_TO_GEMFILE=$(dirname "$RSPEC_FILE_PATH")
+echo 'Gem File path : "${PATH_TO_GEMFILE}"'
 cd "$PATH_TO_GEMFILE"
 run_bundle_install
-
+cd "$GIZA_HOME"
 echo "Start testing of '$RSPEC_FILE_PATH'"
 adb logcat -c
 
