6d594fcfaa62542194bde958edbd951c6a278382
thatb2 <Bhargava.Thatikonda@in.pega.com>
2016-01-22 12:14:09 +0530

    Update run_rspec_andr.sh

--- jenkins_jobs/run_rspec_andr.sh
+++ jenkins_jobs/run_rspec_andr.sh
@@ -77,10 +77,10 @@ set_up_global_properties PROPERTIES
 
 #wget --quiet -O prpcMobileHC.apk $APP_URL
 
-adb kill-server
-sleep 2
-adb start-server
-sleep 5
+#adb kill-server
+#sleep 2
+#adb start-server
+#sleep 5
 #remove_apps_from_device $APP_ID_PREFIX 'Applications removed BEFORE test'
 
 #echo 'Installing app on device'
