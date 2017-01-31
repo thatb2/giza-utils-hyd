fce40242d2a3b043229d22c6a5879a5a18cba782
thatb2 <Bhargava.Thatikonda@in.pega.com>
2017-01-31 16:14:21 +0530

    Update giza_svn_update.sh

--- jenkins_jobs/giza_svn_update.sh
+++ jenkins_jobs/giza_svn_update.sh
@@ -11,12 +11,12 @@ check_dir_exists $GIZA_HOME
 normalize GIZA_HOME
 
 cd $GIZA_HOME
-git checkout master
-git clean -fd
-git checkout .
-git gc --prune
-git status
-git pull origin master
+#git checkout master
+#git clean -fd
+#git checkout .
+#git gc --prune
+#git status
+#git pull origin master
 #svn revert -R .
 #svn update -r HEAD --non-interactive --no-auth-cache --username readonly --password readonly --accept tf
 #svn checkout -r HEAD --non-interactive --no-auth-cache --username readonly --password readonly --accept tf
